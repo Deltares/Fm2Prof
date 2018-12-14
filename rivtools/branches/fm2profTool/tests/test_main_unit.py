613ee62cfbdea99e14ce0bdce2a29e9d7255b01d
soriano <soriano@da8592f4-ba6a-403f-8d83-fa801fd319dd>
2018-12-14 13:24:26 +0000

    Setup of unit and acceptance tags.
    Related to Issue [FMPROF-3]
    
    
    Former-commit-id: 599802e24501aafce158148f8a8d98c7571bc730

--- rivtools/branches/fm2profTool/tests/test_main_unit.py
+++ rivtools/branches/fm2profTool/tests/test_main_unit.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, pytest
 import sys, os
 import TestUtils
 
@@ -6,8 +6,10 @@ class Main_UnitTests(unittest.TestCase):
     def f(self):
         return 4
     
+    @pytest.mark.unittest
     def test_mytest_should_pass(self):
         assert self.f() == 4
     
+    @pytest.mark.unittest
     def test_mytest_should_fail(self):
         assert self.f() == 3
