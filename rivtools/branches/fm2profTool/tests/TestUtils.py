38809b6f4239bc7d8454eb52dd0b98aab07d0f59
soriano <soriano@da8592f4-ba6a-403f-8d83-fa801fd319dd>
2018-12-14 15:13:05 +0000

    Created basic unit and acceptance tests (from here we can start working onwards). Related to Issue [FMPROF-3]

--- rivtools/branches/fm2profTool/tests/TestUtils.py
+++ rivtools/branches/fm2profTool/tests/TestUtils.py
@@ -1,6 +1,6 @@
 import sys, os
 
-def get_test_dir(dirName):
+def get_test_data_dir(dirName):
     """
     Returns the desired directory relative to the test data. 
     Avoiding extra code on the tests.
@@ -8,6 +8,18 @@ def get_test_dir(dirName):
     directory = os.path.dirname(__file__)
     try:
         directory = os.path.join(directory, "test_data\\{0}\\".format(dirName))
+    except:
+        print("An error occurred trying to find {0}".format(dirName))
+    return directory
+
+def get_external_test_data_dir(dirName):
+    """
+    Returns the desired directory relative to the test external data. 
+    Avoiding extra code on the tests.
+    """
+    directory = os.path.dirname(__file__)
+    try:
+        directory = os.path.join(directory, "external_test_data\\{0}\\".format(dirName))
     except:
         print("An error occurred trying to find {0}".format(dirName))
     return directory
