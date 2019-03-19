589c61976e98fdff1ae153c228626dbd6ed14b26
fujisaki <fujisaki@da8592f4-ba6a-403f-8d83-fa801fd319dd>
2019-03-19 10:40:36 +0000

    Related to Issue [FMPROF-12]
    Former-commit-id: 12c6c6cb9411943a5b450c31bdac80845677e7e9

---
+++ testrun_configurationfile.py
@@ -0,0 +1,7 @@
+
+"""  OLD Minimal setup to run fm2prof """
+import os 
+from fm2prof.main import Fm2ProfRunner
+
+runner = Fm2ProfRunner('c:/Projects/KPP/FM2PROF/fm2profTool/fm2prof.ini')
+runner.run()
