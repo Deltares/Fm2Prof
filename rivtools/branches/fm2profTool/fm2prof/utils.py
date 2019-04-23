c64b4799607039cb6351f4f24ec2c91d8b555a45
berend_kn <berend_kn@da8592f4-ba6a-403f-8d83-fa801fd319dd>
2019-04-23 08:29:45 +0000

    Related to Issue [FMPROF-31]
    Former-commit-id: db94aa62110212d92fadeb012745ce33bef5f8bf

---
+++ rivtools/branches/fm2profTool/fm2prof/utils.py
@@ -0,0 +1,77 @@
+"""
+Utilities for FM2PROF 
+"""
+
+import numpy as np 
+
+
+"""
+output is (x, y, id, vaklengte) + (, branch, offset) ??
+
+vaklengte = di+1/2 + di-1/2
+
+"""
+
+def networkdeffile_to_input(networkdefinitionfile, crossectionlocationfile):
+	"""
+	Builds a cross-section input file for FM2PROF from a DIMR network definition file. 
+
+	The distance between cross-section is computed from the differences between the offsets/chainages. 
+	The beginning and end point of each branch are treated as half-distance control volumes. 
+	"""
+
+	# Open network definition file, for each branch extract necessary info
+	x = []
+	y = []
+	cid = []
+	bid = []
+	coff = []
+	cdis = []
+
+	with open(networkdefinitionfile, 'r') as f:
+		for line in f:
+			if line.strip().lower() == "[branch]":
+				branchid = f.readline().split('=')[1].strip()
+				xlength = 0
+				for i in range(10):
+					bline = f.readline().strip().lower().split('=')
+					if bline[0].strip() == "gridpointx":
+						xtmp = list(map(float, bline[1].split()))
+					elif bline[0].strip() == "gridpointy":
+						ytmp = list(map(float, bline[1].split()))
+					elif bline[0].strip() == "gridpointids":
+						cidtmp = bline[1].split(';')
+					elif bline[0].strip() == "gridpointoffsets":
+						cofftmp = list(map(float, bline[1].split()))
+
+						# compute distance between control volumes
+						cdistmp = np.append(np.diff(cofftmp)/2, [0]) + np.append([0], np.diff(cofftmp)/2)
+
+				# Append branchids
+				bid.extend([branchid] * len(xtmp))
+
+				# Correct end points (: at end of branch, gridpoints of this branch and previous branch
+				# occupy the same position, which does not go over well with fm2profs classification algo)
+				offset = 1
+				xtmp[0] = np.interp(offset, cofftmp, xtmp)
+				ytmp[0] = np.interp(offset, cofftmp, ytmp)
+
+				
+				# Append all poitns
+				x.extend(xtmp)
+				y.extend(ytmp)
+				cid.extend(cidtmp)
+				coff.extend(cofftmp)
+				cdis.extend(cdistmp)
+
+
+	with open(crossectionlocationfile, 'w') as f:
+		for i in range(len(x)):
+			f.write('{:.4f}, {:.4f}, {}, {:.2f}, {}, {:.2f}\n'.format(x[i], y[i], cid[i], cdis[i], bid[i], coff[i]))
+
+
+
+
+
+
+
