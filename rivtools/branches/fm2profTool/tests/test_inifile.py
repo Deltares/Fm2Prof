11b3e9273e926d82e7ab4ed7d0c60122824f00b5
soriano <soriano@da8592f4-ba6a-403f-8d83-fa801fd319dd>
2019-07-04 08:26:52 +0000

    Fixed failing test. Related to Issue [FMPROF-36]

--- rivtools/branches/fm2profTool/tests/test_inifile.py
+++ rivtools/branches/fm2profTool/tests/test_inifile.py
@@ -13,7 +13,7 @@ _root_output_dir = None
 
 
 @pytest.mark.unittest
-def test_IniFile_When_No_FilePath_Then_No_Exception_Is_Risen():
+def test_When_No_FilePath_Then_No_Exception_Is_Risen():
     """ 1. Set up initial test data """
     iniFilePath = ''
 
@@ -25,7 +25,7 @@ def test_IniFile_When_No_FilePath_Then_No_Exception_Is_Risen():
 
 
 @pytest.mark.unittest
-def test_IniFile_ReadIniFile_When_No_FilePath_Then_No_Exception_Is_Risen():
+def test_ReadIniFile_When_No_FilePath_Then_No_Exception_Is_Risen():
     """ 1. Set up initial test data """
     iniFilePath = ''
     iniFile = IniFile(iniFilePath)
@@ -36,7 +36,7 @@ def test_IniFile_ReadIniFile_When_No_FilePath_Then_No_Exception_Is_Risen():
 
 
 @pytest.mark.unittest
-def test_IniFile_extract_input_parameters_When_No_Input_Parameters_Key_Returns_None():
+def test_extract_input_parameters_When_No_Input_Parameters_Key_Returns_None():
     # 1. Set up initial test data
     iniFilePath = None
     iniFile = IniFile(iniFilePath)
@@ -53,7 +53,7 @@ def test_IniFile_extract_input_parameters_When_No_Input_Parameters_Key_Returns_N
     assert new_parameters is None
 
 @pytest.mark.unittest
-def test_IniFile_extract_input_parameters_When_No_Parameters_No_Exception_Is_Risen():
+def test_extract_input_parameters_When_No_Parameters_No_Exception_Is_Risen():
         # 1. Set up initial test data
     iniFilePath = None
     iniFile = IniFile(iniFilePath)
@@ -75,7 +75,7 @@ _test_scenarios_input_parameters = [
 
 @pytest.mark.unittest
 @pytest.mark.parametrize("value_as_string, expected_value", _test_scenarios_input_parameters)
-def test_IniFile_extract_input_parameters_When_Parameters_Are_Given_Then_Maps_As_Expected(value_as_string, expected_value):
+def test_extract_input_parameters_When_Parameters_Are_Given_Then_Maps_As_Expected(value_as_string, expected_value):
     # 1. Set up initial test data
     iniFilePath = None
     iniFile = IniFile(iniFilePath)
@@ -100,7 +100,7 @@ def test_IniFile_extract_input_parameters_When_Parameters_Are_Given_Then_Maps_As
     assert isinstance(retievedValue, numbers.Integral) == isinstance(expected_value, numbers.Integral)
 
 @pytest.mark.unittest
-def test_IniFile_extract_input_files_When_No_Input_Parameters_Key_Returns_EmptyDict():
+def test_extract_input_files_When_No_Input_Parameters_Key_Returns_EmptyDict():
     # 1. Set up initial test data
     iniFilePath = None
     iniFile = IniFile(iniFilePath)
@@ -118,7 +118,7 @@ def test_IniFile_extract_input_files_When_No_Input_Parameters_Key_Returns_EmptyD
     assert new_parameters == {}
 
 @pytest.mark.unittest
-def test_IniFile_extract_input_files_gets_filenames():
+def test_extract_input_files_gets_filenames():
     # 1. Set up initial test data
     iniFilePath = None
     new_parameters = None
@@ -143,7 +143,7 @@ def test_IniFile_extract_input_files_gets_filenames():
     assert new_parameters[file_key] == file_value
 
 @pytest.mark.unittest
-def test_IniFile_extract_output_dir_no_outputdir_key_returns_None():
+def test_extract_output_dir_no_outputdir_key_returns_None():
     # 1. Set initial test data
     ini_file_path = None
     iniFile = IniFile(ini_file_path)
@@ -169,7 +169,7 @@ _test_scenarios_output_dir_cases = [
 
 @pytest.mark.integrationtest
 @pytest.mark.parametrize("param_output_dir_value, param_case_name_value, expected_value", _test_scenarios_output_dir_cases)
-def test_IniFile_extract_output_dir_gets_correct_path(param_output_dir_value, param_case_name_value, expected_value):
+def test_extract_output_dir_gets_correct_path(param_output_dir_value, param_case_name_value, expected_value):
     # 1. Set initial test data
     ini_file_path = None
     iniFile = IniFile(ini_file_path)
@@ -204,7 +204,7 @@ _test_scenarios_case_names = [
 
 @pytest.mark.unittest
 @pytest.mark.parametrize("case_name, output_dir, expected_value", _test_scenarios_case_names)
-def test_IniFile_get_valid_case_name_returns_expected_values(case_name, output_dir, expected_value):
+def test_get_valid_case_name_returns_expected_values(case_name, output_dir, expected_value):
     # 1. Set initial test data
     ini_file_path = None
     iniFile = IniFile(ini_file_path)
@@ -220,6 +220,39 @@ def test_IniFile_get_valid_case_name_returns_expected_values(case_name, output_d
     assert new_case_name == expected_value
 
 
+@pytest.mark.develop
+def test_get_valid_case_name_returns_new_values_for_existent_dirs():
+     # 1. Set initial test data
+    ini_file_path = None
+    iniFile = IniFile(ini_file_path)
+    repeated_iterations = 5
+    case_name = 'dummyCaseName'
+    output_dir = 'IniFileTests'
+    case_names = []
+
+    # 2. Verify initial expectations
+    if os.path.isdir(output_dir):
+        shutil.rmtree(output_dir)
+    os.mkdir(output_dir)
+    
+    # 3. Run test
+    try:
+        for idx in range(0, repeated_iterations):
+            new_case_name = iniFile._get_valid_case_name(case_name, output_dir)
+            relative_dir = os.path.join(output_dir, new_case_name)
+            os.mkdir(relative_dir)
+            case_names.append(new_case_name)
+    except:
+        shutil.rmtree(output_dir)
+        pytest.fail('Test failed while trying to get valid case names.')
+    
+    # 4. Verify final expectations
+    shutil.rmtree(output_dir)
+    assert case_names
+    assert len(case_names) == repeated_iterations
+    set_case_names = set(case_names)
+    assert len(set_case_names) == repeated_iterations
+
 _test_scenarios_output_dirs = [
     ('dummydir', 'dummydir'),
     ('dummydir/dummysubdir', 'dummydir\\dummysubdir'),
@@ -228,7 +261,7 @@ _test_scenarios_output_dirs = [
 
 @pytest.mark.unittest
 @pytest.mark.parametrize("output_dir, expected_value", _test_scenarios_output_dirs)
-def test_IniFile_get_valid_output_dir_returns_expected_values(output_dir, expected_value):
+def test_get_valid_output_dir_returns_expected_values(output_dir, expected_value):
     # 1. Set initial test data
     ini_file_path = None
     iniFile = IniFile(ini_file_path)
@@ -244,7 +277,7 @@ def test_IniFile_get_valid_output_dir_returns_expected_values(output_dir, expect
     assert expected_value in new_output_dir
 
 @pytest.mark.unittest
-def test_IniFile_get_valid_output_dir_when_no_output_dir_returns_current_path():
+def test_get_valid_output_dir_when_no_output_dir_returns_current_path():
     # 1. Set initial test data
     ini_file_path = None
     iniFile = IniFile(ini_file_path)
@@ -261,7 +294,7 @@ def test_IniFile_get_valid_output_dir_when_no_output_dir_returns_current_path():
     assert expected_value == new_output_dir
 
 @pytest.mark.systemtest
-def test_IniFile_read_ini_file_sets_output_dir():
+def test_read_ini_file_sets_output_dir():
     # 1. Set initial test data
     test_data_dir = TestUtils.get_test_data_dir('IniFile')
     ini_file_path = os.path.join(test_data_dir, 'valid_ini_file.ini')
@@ -288,7 +321,7 @@ def test_IniFile_read_ini_file_sets_output_dir():
     assert format_output_dir == expected_output_dir
 
 @pytest.mark.systemtest
-def test_IniFile_read_ini_file_sets_input_parameters():
+def test_read_ini_file_sets_input_parameters():
     # 1. Set initial test data
     test_data_dir = TestUtils.get_test_data_dir('IniFile')
     ini_file_path = os.path.join(test_data_dir, 'valid_ini_file.ini')
@@ -325,7 +358,7 @@ def test_IniFile_read_ini_file_sets_input_parameters():
         assert read_param_value == expected_value, 'Expected value does not match for key {}'.format(expected_input_param)
 
 @pytest.mark.systemtest
-def test_IniFile_read_ini_file_sets_input_file_paths():
+def test_read_ini_file_sets_input_file_paths():
     # 1. Set initial test data
     test_data_dir = TestUtils.get_test_data_dir('IniFile')
     ini_file_path = os.path.join(test_data_dir, 'valid_ini_file.ini')
