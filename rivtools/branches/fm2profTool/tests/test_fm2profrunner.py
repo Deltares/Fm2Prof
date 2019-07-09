23487d976edcb31012117dc8cf625c2084d17f16
soriano <soriano@da8592f4-ba6a-403f-8d83-fa801fd319dd>
2019-07-09 12:37:52 +0000

    Fixed failing tests. Adapted some code to PEP8 standard.
    Related to Issue [FMPROF-37]
    
    
    Former-commit-id: afe0b4cff138cdef91fed80f13f8f586b44fca0b

--- rivtools/branches/fm2profTool/tests/test_fm2profrunner.py
+++ rivtools/branches/fm2profTool/tests/test_fm2profrunner.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import shutil
 
-import TestUtils
+from tests import TestUtils
 from fm2prof.Fm2ProfRunner import Fm2ProfRunner as Fm2ProfRunner
 from fm2prof.IniFile import IniFile
 from fm2prof.Classes import CrossSection as CS
