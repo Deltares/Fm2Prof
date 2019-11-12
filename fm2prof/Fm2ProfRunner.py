92a9695c4ad307fbdc7290a63bde5a2a9292de0d
Carles S. Soriano Pérez <sorianoperez.carles@gmail.com>
2019-11-12 08:07:04 +0000

    Added license headers and both gpl and lgpl
    Former-commit-id: e73dd58bc3930a9c08534164140cafc97b1a7e65

--- fm2prof/Fm2ProfRunner.py
+++ fm2prof/Fm2ProfRunner.py
@@ -16,6 +16,28 @@ matplotlib (1.5.1)
 
 Contact: K.D. Berends (koen.berends@deltares.nl, k.d.berends@utwente.nl)
 """
+"""
+Copyright (C) Stichting Deltares 2019. All rights reserved.
+
+This file is part of the Fm2Prof.
+
+The Fm2Prof is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+All names, logos, and references to "Deltares" are registered trademarks of
+Stichting Deltares and remain full property of Stichting Deltares at all times.
+All rights reserved.
+"""
 
 from fm2prof import Functions as FE
 from fm2prof import Classes as CE
