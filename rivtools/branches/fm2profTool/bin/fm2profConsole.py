8459aae74891aebb49b6d14ac3daf5e5630fb018
berend_kn <berend_kn@da8592f4-ba6a-403f-8d83-fa801fd319dd>
2020-07-27 09:02:56 +0000

    Related to Issue [FMPROF-136]

--- rivtools/branches/fm2profTool/bin/fm2profConsole.py
+++ rivtools/branches/fm2profTool/bin/fm2profConsole.py
@@ -1,12 +1,11 @@
 # Import from standard library
-import re
+
 
 # Import from dependencies
 import click
 
 # Import from package
-from fm2prof.main import Fm2ProfRunner
-import fm2prof.utils as fmu
+from fm2prof import Project
 from fm2prof.IniFile import IniFile
 
 
@@ -14,13 +13,14 @@ from fm2prof.IniFile import IniFile
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('-f', help='Load configuration file')
-@click.option('-n', help='Create empty ini file in current with name')
-@click.option('-r', help='Run program (always use in combination with 0f)', is_flag=True)
-def __cli(**kwargs):
+@click.option('-f', '--fileload', help='Load configuration file')
+@click.option('-n', '--new', help='Create empty ini file in current with name')
+@click.option('-r', '--run', help='Run program (always use in combination with -f)', is_flag=True)
+
+def cli(**kwargs):
     project = None
     if kwargs.get('f'):
-        project = Fm2ProfRunner.Fm2ProfRunner(kwargs.get('f'))
+        project = Project(kwargs.get('f'))
     if kwargs.get('n'):
         # Create empty project based on inifile
         inifile = IniFile().print_configuration()
@@ -31,10 +31,10 @@ def __cli(**kwargs):
             f.write(inifile)
         click.echo(f"{ini_path} written to file")
     if kwargs.get('r'):
-    	click.echo(project.run())
+        click.echo(project.run())
     else:
-        with click.Context(__cli) as ctx:
+        with click.Context(cli) as ctx:
             click.echo(ctx.get_help())
 
 if __name__ == "__main__":
-    __cli()
+    cli()
