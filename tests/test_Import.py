bf56fcad9b7755de282538053f67303d59da56ed
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-11-29 21:09:09 +0100

    ci: Migration to GitHub (#1)
    * ci: Added pipelines and replaced poetry as package handler
    
    * ci: Updated pipelines and version references.
    
    * ci(pyproject.toml): Updated version reference in commitizen field
    
    * ci(poetry.lock): Updated poetry files
    
    * ci(pyproject.toml): Stable toml without matplotlib
    
    * ci(pyproject.toml): Updated toml with matplotlib dependency
    
    * ci(.pre-commit-config.yaml): Added pre-commit hook to git repo
    
    * ci(.github/workflows/docs.yml): Modified docs pipeline to build sphinx docs
    
    * ci(__init__.py): Corrected silly typo
    
    * test(tests/): Fixed some failing tests due to local directories and path handling
    
    * test(tests/): Ignored tests which contain sensible data
    
    * test(tests/test_data): Added test data files
    
    * ci(pyproject.toml): Changed dependency python into 3.7 for win run
    
    * ci(pyproject.tom;poetry.lock): Added poetry2conda dependency to forge correctly
    
    * ci(tests/): Removed duplicated dependency on environment.yml, added shapely forge
    
    * ci(pyproject.toml): Made netcdf4 version fixed
    
    Former-commit-id: 8f384a7a7e4cbc6c05064eab8fce324a62e0b749

--- tests/test_Import.py
+++ tests/test_Import.py
@@ -5,38 +5,37 @@ import os
 import numbers
 from pathlib import Path
 import shutil
-from tests.TestUtils import TestUtils
+from tests.TestUtils import TestUtils, skipwhenexternalsmissing
 
 from fm2prof.Import import FmModelData, FMDataImporter
 
 
 class Test_FMDataImporter:
     @pytest.mark.unittest
+    @skipwhenexternalsmissing
     def test_when_map_file_without_czu_no_exception(self):
         # 1. Set up test data
-        test_map = Path(TestUtils.get_local_test_data_dir("main_test_data")).joinpath("fm_map.nc")
+        test_map = Path(TestUtils.get_local_test_data_dir("main_test_data")).joinpath(
+            "fm_map.nc"
+        )
+        assert test_map.is_file()
 
         # 2. Set initial expectations
+        return_value = FMDataImporter().import_dflow2d(test_map)
 
-        # 3. Run test
-        try:
-            FMDataImporter().import_dflow2d(test_map)
-        except:
-            pytest.fail('No exception expected but was thrown')
-        
-        # 4. Verify final expectations
-        
+        # 3. Verify final expectations
+        assert return_value is not None
+
 
 class Test_FmModelData:
-
     @pytest.mark.unittest
-    @pytest.mark.parametrize("arg_list", [(''), (None)])
+    @pytest.mark.parametrize("arg_list", [(""), (None)])
     def test_when_no_arguments_exception_is_risen(self, arg_list):
         # 1. Set up test data
         arg_list = arg_list
 
         # 2. Set initial expectations
-        expected_error_message = 'FM model data was not read correctly.'
+        expected_error_message = "FM model data was not read correctly."
 
         # 3. Run test
         with pytest.raises(Exception) as pytest_wrapped_e:
@@ -44,21 +43,26 @@ class Test_FmModelData:
 
         # 4. Verify final expectations
         recieved_error_message = str(pytest_wrapped_e.value)
-        assert expected_error_message == recieved_error_message, '' + \
-            'Expected error message {},'.format(expected_error_message) + \
-            ' does not match generated {}'.format(recieved_error_message)
+        assert expected_error_message == recieved_error_message, (
+            ""
+            + "Expected error message {},".format(expected_error_message)
+            + " does not match generated {}".format(recieved_error_message)
+        )
 
     @pytest.mark.unittest
-    @pytest.mark.parametrize("arg_list", [(''), (None)])
+    @pytest.mark.parametrize("arg_list", [(""), (None)])
     def test_when_argument_length_not_as_expected_then_exception_is_risen(
-            self, arg_list):
+        self, arg_list
+    ):
         # 1. Set up test data
-        arg_list = ['arg1', 'arg2']
+        arg_list = ["arg1", "arg2"]
 
         # 2. Set initial expectations
-        expected_error_message = '' + \
-            'Fm model data expects 5 arguments but only' + \
-            ' {} were given'.format(len(arg_list))
+        expected_error_message = (
+            ""
+            + "Fm model data expects 5 arguments but only"
+            + " {} were given".format(len(arg_list))
+        )
 
         # 3. Run test
         with pytest.raises(Exception) as pytest_wrapped_e:
@@ -66,36 +70,38 @@ class Test_FmModelData:
 
         # 4. Verify final expectations
         recieved_error_message = str(pytest_wrapped_e.value)
-        assert expected_error_message == recieved_error_message, '' + \
-            'Expected error message {},'.format(expected_error_message) + \
-            ' does not match generated {}'.format(recieved_error_message)
+        assert expected_error_message == recieved_error_message, (
+            ""
+            + "Expected error message {},".format(expected_error_message)
+            + " does not match generated {}".format(recieved_error_message)
+        )
 
     @pytest.mark.unittest
     def test_when_given_expected_arguments_then_object_is_created(self):
         # 1. Set up test data
-        time_dependent_data = 'arg1'
-        time_independent_data = 'arg2'
-        edge_data = 'arg3'
-        node_coordinates = 'arg4'
-        css_data = 'arg5'
+        time_dependent_data = "arg1"
+        time_independent_data = "arg2"
+        edge_data = "arg3"
+        node_coordinates = "arg4"
+        css_data = "arg5"
         arg_list = [
             time_dependent_data,
             time_independent_data,
             edge_data,
             node_coordinates,
-            css_data]
+            css_data,
+        ]
         return_fm_model_data = None
         # 2. Run test
         try:
             return_fm_model_data = FmModelData(arg_list)
         except:
-            pytest.fail('No exception expected but was thrown')
+            pytest.fail("No exception expected but was thrown")
 
         # 4. Verify final expectations
         assert return_fm_model_data is not None
         assert return_fm_model_data.time_dependent_data == time_dependent_data
-        assert return_fm_model_data.time_independent_data \
-            == time_independent_data
+        assert return_fm_model_data.time_independent_data == time_independent_data
         assert return_fm_model_data.edge_data == edge_data
         assert return_fm_model_data.node_coordinates == node_coordinates
         assert return_fm_model_data.css_data_list == []
@@ -103,11 +109,11 @@ class Test_FmModelData:
     @pytest.mark.unittest
     def test_when_given_data_dictionary_then_css_data_list_is_set(self):
         # 1. Set up test data
-        time_dependent_data = 'arg1'
-        time_independent_data = 'arg2'
-        edge_data = 'arg3'
-        node_coordinates = 'arg4'
-        dummy_key = 'dummyKey'
+        time_dependent_data = "arg1"
+        time_independent_data = "arg2"
+        edge_data = "arg3"
+        node_coordinates = "arg4"
+        dummy_key = "dummyKey"
         dummy_values = [0, 1]
         css_data_dict = {
             dummy_key: dummy_values,
@@ -118,7 +124,8 @@ class Test_FmModelData:
             time_independent_data,
             edge_data,
             node_coordinates,
-            css_data_dict]
+            css_data_dict,
+        ]
         return_fm_model_data = None
 
         # 2. Set expectations
@@ -128,7 +135,7 @@ class Test_FmModelData:
         try:
             return_fm_model_data = FmModelData(arg_list)
         except:
-            pytest.fail('No exception expected but was thrown')
+            pytest.fail("No exception expected but was thrown")
 
         # 4. Verify final expectations
         assert return_fm_model_data is not None
@@ -137,12 +144,11 @@ class Test_FmModelData:
 
 
 class Test_get_ordered_css_list:
-
     @pytest.mark.unittest
     def test_when_given_dictionary_then_returns_list(self):
         # 1. Set up test_data
         return_list = None
-        dummy_key = 'dummyKey'
+        dummy_key = "dummyKey"
         dummy_values = [0, 1]
         test_dict = {
             dummy_key: dummy_values,
@@ -155,21 +161,22 @@ class Test_get_ordered_css_list:
         try:
             return_list = FmModelData.get_ordered_css_list(test_dict)
         except:
-            pytest.fail('No exception expected but was thrown')
+            pytest.fail("No exception expected but was thrown")
 
         # 4. Verify final expectations
         assert return_list is not None
-        assert return_list == expected_list, '' + \
-            'Expected return value {},'.format(expected_list) + \
-            ' but return {} instead.'.format(return_list)
+        assert return_list == expected_list, (
+            ""
+            + "Expected return value {},".format(expected_list)
+            + " but return {} instead.".format(return_list)
+        )
 
     @pytest.mark.unittest
-    @pytest.mark.parametrize('test_dict', [(''), (None), ({})])
-    def test_when_given_unexpected_value_then_returns_empty_list(
-            self, test_dict):
+    @pytest.mark.parametrize("test_dict", [(""), (None), ({})])
+    def test_when_given_unexpected_value_then_returns_empty_list(self, test_dict):
         # 1. Set up test_data
         return_list = None
-        dummy_key = 'dummyKey'
+        dummy_key = "dummyKey"
         dummy_values = [0, 1]
 
         # 2. Run test
@@ -179,10 +186,12 @@ class Test_get_ordered_css_list:
         try:
             return_list = FmModelData.get_ordered_css_list(test_dict)
         except:
-            pytest.fail('No exception expected but was thrown')
+            pytest.fail("No exception expected but was thrown")
 
         # 4. Verify final expectations
         assert return_list is not None
-        assert return_list == expected_list, '' + \
-            'Expected return value {},'.format(expected_list) + \
-            ' but return {} instead.'.format(return_list)
+        assert return_list == expected_list, (
+            ""
+            + "Expected return value {},".format(expected_list)
+            + " but return {} instead.".format(return_list)
+        )
