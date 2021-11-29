81a4e2f33a73432e2653caf4d3af8f47ea6852fa
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-11-29 21:33:42 +0100

    docs(docs/fm2prof_docs): Removed outdated latex files

--- docs/fm2prof_docs/Fm2prof - Test Plan/buildDoc.bat
+++
@@ -1,6 +0,0 @@
-pdflatex "Fm2prof - Test Plan.tex"
-bibtex "Fm2prof - Test Plan"
-pdflatex "Fm2prof - Test Plan.tex"
-pdflatex "Fm2prof - Test Plan.tex" > FM2PROF_TP_Log.txt
-xcopy FM2PROF_TP_Log.txt "..\BuildLogs" /Y
-xcopy *.pdf .. /Y
