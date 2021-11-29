55095d6ff7e944dd07f9c0c2bf876e56fecfa3b7
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-11-29 21:33:42 +0100

    docs(docs/fm2prof_docs): Removed outdated latex files
    Former-commit-id: 81a4e2f33a73432e2653caf4d3af8f47ea6852fa

--- docs/fm2prof_docs/Fm2prof - Manual/buildDoc.bat
+++
@@ -1,6 +0,0 @@
-pdflatex "Fm2prof - Manual.tex"
-bibtex "Fm2prof - Manual"
-pdflatex "Fm2prof - Manual.tex"
-pdflatex "Fm2prof - Manual.tex" > FM2PROF_MAN_Log.txt
-xcopy FM2PROF_MAN_Log.txt "..\BuildLogs" /Y
-xcopy *.pdf .. /Y
--- docs/fm2prof_docs/Fm2prof - Test Report/buildDoc.bat
+++
@@ -1,6 +0,0 @@
-pdflatex "Fm2prof - Test Report.tex"
-bibtex "Fm2prof - Test Report"
-pdflatex "Fm2prof - Test Report.tex"
-pdflatex "Fm2prof - Test Report.tex" > FM2PROF_TR_Log.txt
-xcopy FM2PROF_TR_Log.txt "..\BuildLogs" /Y
-xcopy *.pdf .. /Y
