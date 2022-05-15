dc998690fc31924394a6c138b1430c8d27a74a5f
kdberends <kdberends@users.noreply.github.com>
2022-05-15 19:09:31 +0000

    autoformat: isort & black
    Former-commit-id: bdcd7df1f6c3214bebd97eeddc0a61e2e7e5bfe2

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,21 +1,24 @@
-from typer.testing import CliRunner
 from pathlib import Path
-from fm2prof.cli import app 
+
+from typer.testing import CliRunner
+
+from fm2prof.cli import app
 
 runner = CliRunner()
-new_name = 'testIni'
+new_name = "testIni"
+
 
 def test_app():
-    result = runner.invoke(app, '--help')
+    result = runner.invoke(app, "--help")
     assert result.exit_code == 0
 
+
 def test_app_create_new_file():
-    result = runner.invoke(app, f'create {new_name}')
+    result = runner.invoke(app, f"create {new_name}")
     assert result.exit_code == 0
     assert Path(f"{new_name}.ini").is_file()
 
+
 def test_app_check_project():
-    result = runner.invoke(app, f'check {new_name}.ini')
+    result = runner.invoke(app, f"check {new_name}.ini")
     assert result.exit_code == 0
-
-
