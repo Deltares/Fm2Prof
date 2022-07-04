29dcbf369c080df8fb7f40c038b73fe025901c46
kdberends <kdberends@users.noreply.github.com>
2022-07-04 06:29:11 +0000

    autoformat: isort & black
    Former-commit-id: cdd603917c785d94091c0b5ab55281c6378c54d9

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,39 +6,47 @@ import unittest
 
 import pytest
 
-from fm2prof.utils import GenerateCrossSectionLocationFile, DeltaresConfig, ModelOutputReader
+from fm2prof.utils import (
+    DeltaresConfig,
+    GenerateCrossSectionLocationFile,
+    ModelOutputReader,
+)
 from tests.TestUtils import TestUtils
+
 _root_output_dir = None
 
 
 class Test_DeltaresConfig:
-
     def test_given_input_file_is_read(self):
         # 1. Set up initial test data
-        ini_file_path = TestUtils.get_local_test_file('cases/case_02_compound/Model_SOBEK/dimr/dflow1d/NetworkDefinition.ini')
+        ini_file_path = TestUtils.get_local_test_file(
+            "cases/case_02_compound/Model_SOBEK/dimr/dflow1d/NetworkDefinition.ini"
+        )
 
         # 2. Set expectations
-        dict_has_keys = ['general', 'node', 'branch']
+        dict_has_keys = ["general", "node", "branch"]
 
         # 3. Run test
         parsed_dict = DeltaresConfig(ini_file_path)
 
         # 4. Verify final expectations
         for key in dict_has_keys:
-            assert(key in parsed_dict.sections)
+            assert key in parsed_dict.sections
+
 
 class Test_ModelOutputReader:
     def DUMMY_test_given_1dinput_csv_generated(self):
         # 1. Set up initial test data
-        path_1d = TestUtils.get_local_test_file('cases/case_02_compound/Model_SOBEK/dimr/dflow1d/NetworkDefinition.ini')
+        path_1d = TestUtils.get_local_test_file(
+            "cases/case_02_compound/Model_SOBEK/dimr/dflow1d/NetworkDefinition.ini"
+        )
 
         # 2. Set expectations
-        
+
         # 3. Run test
-        output = ModelOutputReader.path_flow1d 
+        output = ModelOutputReader.path_flow1d
         output.path_flow1d = path_1d
         output.path_flow2d = path_2d
-
 
         output.load_flow1d_data()
         output.get_1d2d_map()
@@ -46,4 +54,4 @@ class Test_ModelOutputReader:
 
         # 4. Verify final expectations
         for key in dict_has_keys:
-            assert(key in parsed_dict.sections)
+            assert key in parsed_dict.sections
