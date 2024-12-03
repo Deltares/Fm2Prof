ddf825e1140c1e1793d512e78cea3dcc262548fe
Tjalling-dejong <tjalling.dejong@deltares.nl>
2024-12-03 16:28:37 +0100

    renamed Export to export.py

--- fm2prof/Fm2ProfRunner.py
+++ fm2prof/Fm2ProfRunner.py
@@ -16,7 +16,7 @@ from fm2prof import Functions as FE
 from fm2prof import __version__
 from fm2prof.common import FM2ProfBase
 from fm2prof.cross_section import CrossSection, CrossSectionHelpers
-from fm2prof.Export import Export1DModelData, OutputFiles
+from fm2prof.export import Export1DModelData, OutputFiles
 from fm2prof.Import import FMDataImporter, FmModelData, ImportInputFiles
 from fm2prof.ini_file import IniFile
 from fm2prof.MaskOutputFile import MaskOutputFile
