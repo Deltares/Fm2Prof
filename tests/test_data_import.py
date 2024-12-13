c0ea2895a1ecbb6e7d1938fcca110a30587b7230
Tjalling-dejong <tjalling.dejong@deltares.nl>
2024-12-13 10:42:24 +0100

    removed unnecessary tests

--- tests/test_data_import.py
+++ tests/test_data_import.py
@@ -21,48 +21,6 @@ class Test_FMDataImporter:
 
 
 class Test_FmModelData:
-    @pytest.mark.parametrize("arg_list", [(""), (None)])
-    def test_when_no_arguments_exception_is_risen(self, arg_list):
-        # 1. Set up test data
-        arg_list = arg_list
-
-        # 2. Set initial expectations
-        expected_error_message = "FM model data was not read correctly."
-
-        # 3. Run test
-        with pytest.raises(Exception) as pytest_wrapped_e:
-            FmModelData(arg_list)
-
-        # 4. Verify final expectations
-        recieved_error_message = str(pytest_wrapped_e.value)
-        assert expected_error_message == recieved_error_message, (
-            ""
-            + "Expected error message {},".format(expected_error_message)
-            + " does not match generated {}".format(recieved_error_message)
-        )
-
-    @pytest.mark.parametrize("arg_list", [(""), (None)])
-    def test_when_argument_length_not_as_expected_then_exception_is_risen(self, arg_list):
-        # 1. Set up test data
-        arg_list = ["arg1", "arg2"]
-
-        # 2. Set initial expectations
-        expected_error_message = (
-            "" + "Fm model data expects 5 arguments but only" + " {} were given".format(len(arg_list))
-        )
-
-        # 3. Run test
-        with pytest.raises(Exception) as pytest_wrapped_e:
-            FmModelData(arg_list)
-
-        # 4. Verify final expectations
-        recieved_error_message = str(pytest_wrapped_e.value)
-        assert expected_error_message == recieved_error_message, (
-            ""
-            + "Expected error message {},".format(expected_error_message)
-            + " does not match generated {}".format(recieved_error_message)
-        )
-
     def test_when_given_expected_arguments_then_object_is_created(self):
         # 1. Set up test data
         time_dependent_data = "arg1"
@@ -70,17 +28,16 @@ class Test_FmModelData:
         edge_data = "arg3"
         node_coordinates = "arg4"
         css_data = "arg5"
-        arg_list = [
-            time_dependent_data,
-            time_independent_data,
-            edge_data,
-            node_coordinates,
-            css_data,
-        ]
         return_fm_model_data = None
 
         # 2. Run test
-        return_fm_model_data = FmModelData(arg_list)
+        return_fm_model_data = FmModelData(
+            time_dependent_data=time_dependent_data,
+            time_independent_data=time_independent_data,
+            edge_data=edge_data,
+            node_coordinates=node_coordinates,
+            css_data_dictionary=css_data,
+        )
 
         # 4. Verify final expectations
         assert return_fm_model_data is not None
@@ -102,20 +59,19 @@ class Test_FmModelData:
             dummy_key: dummy_values,
         }
 
-        arg_list = [
-            time_dependent_data,
-            time_independent_data,
-            edge_data,
-            node_coordinates,
-            css_data_dict,
-        ]
         return_fm_model_data = None
 
         # 2. Set expectations
         expected_css_data_list = [{dummy_key: 0}, {dummy_key: 1}]
 
         #  3. Run test
-        return_fm_model_data = FmModelData(arg_list)
+        return_fm_model_data = FmModelData(
+            time_dependent_data=time_dependent_data,
+            time_independent_data=time_independent_data,
+            edge_data=edge_data,
+            node_coordinates=node_coordinates,
+            css_data_dictionary=css_data_dict,
+        )
 
         # 4. Verify final expectations
         assert return_fm_model_data is not None
