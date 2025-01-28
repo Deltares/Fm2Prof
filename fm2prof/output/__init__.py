63c2f01bf20c16ab01f8b45843bf328f0bc7bca1
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-01-28 17:03:21 +0100

    moved namedtuples to typing file

---
+++ fm2prof/output/__init__.py
@@ -0,0 +1,27 @@
+"""Functions and classes for reading and analyzing Fm2Prof output."""
+
+
+COLORSCHEMES = {
+    "Deltares": ["#000000", "#00cc96", "#0d38e0"],
+    "Koeln": [
+        "#000000",
+        "#E69F00",
+        "#56B4E9",
+        "#009E73",
+        "#F0E444",
+        "#0072B2",
+        "#D55E00",
+        "#CC79A7",
+    ],  # https://jfly.uni-koeln.de/color/
+    "PaulTolVibrant": [
+        "#0077BB",
+        "#33BBEE",
+        "#009988",
+        "#EE7733",
+        "#CC3311",
+        "#EE3377",
+        "#BBBBBB",
+    ],
+}
+
+
---
+++ fm2prof/output/_types.py
@@ -0,0 +1,16 @@
+from typing import NamedTuple
+
+import matplotlib as mpl
+
+
+class FigureOutput(NamedTuple):
+    fig: mpl.figure.Figure
+    axes: mpl.axes.Axes
+    legend: mpl.legend.Legend
+
+class StyleGuide(NamedTuple):
+    font: dict
+    major_grid: dict
+    minor_grid: dict
+    spine_width: float
+
